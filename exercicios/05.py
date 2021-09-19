79092f30f085a937cbc8e02d1187021225b7d968
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2021-09-19 18:16:01 -0300

    Create 05.py

---
+++ exercicios/05.py
@@ -0,0 +1,4 @@
+# Pega primeiro e último nome do usuário, retorna invertidos e com espaço entre eles
+primeiro = (input ("Digite o primeiro nome: "))
+ultimo = (input ("Digite o último nome: "))
+print (f'{primeiro[::-1]} {ultimo[::-1]}')
