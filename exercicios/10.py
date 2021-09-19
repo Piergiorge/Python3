fb2b9a13afbe0ae1781814d7ab6c8f81d7e38694
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2021-09-19 18:23:50 -0300

    Create 10.py

---
+++ exercicios/10.py
@@ -0,0 +1,6 @@
+# aceita um valor (n) e calcula a soma de n + nn + nnn
+n = input("Digite o valor: ")
+calculo1 = int((n*1))
+calculo2 = int((n*2))
+calculo3 = int((n*3))
+print (calculo1+calculo2+calculo3)
