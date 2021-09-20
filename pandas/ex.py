6a243b1b9f74488ebe42b3c9d5261274193c15ce
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2021-09-19 23:19:18 -0300

    Create ex.py

---
+++ pandas/ex.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as srn
+import statistics as sts
+from sklearn import datasets
+# Load and return the boston house-prices dataset (regression).
+data = datasets.load_boston()
+toy = pd.DataFrame(data["data"], columns=data["feature_names"])
+display(toy)
+print(toy.shape)
+agrupado = toy.groupby(['AGE'])
+
+agrupado = toy.groupby(['AGE']).size()
+display(agrupado)
+toy['AGE'].describe()
+
+# outliers
+toy.loc[(toy['AGE'] < 0) | (toy['AGE'] > 120)]
+
+# NA
+toy.isnull().sum()
