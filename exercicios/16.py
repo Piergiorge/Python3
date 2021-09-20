857b140321cdaf970b3b5a52c60219a8d4a8158d
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2021-09-19 21:18:49 -0300

    Update 16.py

--- exercicios/16.py
+++ exercicios/16.py
@@ -1,5 +1,5 @@
+# carrega um inteiro, verifica a diferenca em relacao a 17, verifica se essa diferença é maior do que 17 e retorna duas vezes o valor absoluto
 num = int(input ('Digite um número: '))
 diferenca = num - 17
 if (diferenca > 17):
-    print (f'{diferenca} - {num}')
     print (abs(diferenca)*2)
